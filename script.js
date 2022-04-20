3b06cf3026a0a33d41750d7d1d29d7900afd8a62
Definitely not xl_83 <98920372+LX8369@users.noreply.github.com>
2022-04-20 10:52:39 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const websites = "{
+const websites = {
 google:{name:"Google", text:"Google", icon:"https://www.google.com/favicon.ico"},
 googleClassroom:{name:"Google Classroom", text:"Courses", icon:"https://ssl.gstatic.com/classroom/favicon.png"},
 outlook:{name:"outlook", text:"Outlook", icon:"https://outlook.office365.com/owa/favicon.ico"},
 googleDrive:{name:"google drive", text:"My Drive", icon:"https://ssl.gstatic.com/images/branding/product/1x/drive_2020q4_32dp.png"},
-camMath:{name:"cambridge math", text:"Cambridge Essential Math, icon:'https://emac.hotmaths.com.au/users/branding/emacs/img/favicon.ico'}
-}"
+camMath:{name:"cambridge math", text:"Cambridge Essential Math", icon:'https://emac.hotmaths.com.au/users/branding/emacs/img/favicon.ico'}
+}
 
 const favicon = document.querySelector('[rel=icon]');
