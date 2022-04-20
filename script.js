4f811d6296ffc47ee6857a4ceba7859e63aecbed
Definitely not xl_83 <98920372+LX8369@users.noreply.github.com>
2022-04-20 11:07:53 +1000

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,16 @@ outlook:{name:"outlook", text:"Outlook", icon:"https://outlook.office365.com/owa
 googleDrive:{name:"google drive", text:"My Drive", icon:"https://ssl.gstatic.com/images/branding/product/1x/drive_2020q4_32dp.png"},
 camMath:{name:"cambridge math", text:"Cambridge Essential Math", icon:'https://emac.hotmaths.com.au/users/branding/emacs/img/favicon.ico'}
 }
-
+const parsedData = JSON.parse(websites)
 const favicon = document.querySelector('[rel=icon]');
 
-input("Please insert a website (A list of websites can be found on the github page")
+function ask() {
+  var site = prompt("Please insert a website (A list of websites can be found on the github page)")
+  var ans = site.toLowerCase()
+
+if (site =! null) {
+  Object.keys(parsedData).every(function(key) {
+      console.log(key)
+  }
+}
+}
