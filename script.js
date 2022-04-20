9abc980757ceb7e27d9bad096879dbb1b7f672fc
Definitely not xl_83 <98920372+LX8369@users.noreply.github.com>
2022-04-21 08:59:02 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const websites = {
-"google":{"name":"Google", "text":"Google", "icon":"https://www.google.com/favicon.ico"},
-"googleClassroom":{"name":"Google Classroom", "text":"Courses", "icon":"https://ssl.gstatic.com/classroom/favicon.png"},
+"google":{"name":"google", "text":"Google", "icon":"https://www.google.com/favicon.ico"},
+"googleClassroom":{"name":"google glassroom", "text":"Courses", "icon":"https://ssl.gstatic.com/classroom/favicon.png"},
 "outlook":{"name":"outlook", "text":"Outlook", "icon":"https://outlook.office365.com/owa/favicon.ico"},
 "googleDrive":{"name":"google drive", "text":"My Drive", "icon":"https://ssl.gstatic.com/images/branding/product/1x/drive_2020q4_32dp.png"},
 "camMath":{"name":"cambridge math", "text":"Cambridge Essential Math", "icon":"https://emac.hotmaths.com.au/users/branding/emacs/img/favicon.ico"}
