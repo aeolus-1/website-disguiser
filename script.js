9fef6ef90906b3bf2e9e927206d868a4fd0ebd6f
aeolus-1 <102002218+aeolus-1@users.noreply.github.com>
2022-05-04 11:09:44 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const websites = {
 "google":{"name":"google", "text":"Google", "icon":"https://www.google.com/favicon.ico"},
-"googleClassroom":{"name":"google classroom", "text":"Courses", "icon":"https://ssl.gstatic.com/classroom/favicon.png"},
+"googleclassroom":{"name":"google classroom", "text":"Courses", "icon":"https://ssl.gstatic.com/classroom/favicon.png"},
 "outlook":{"name":"outlook", "text":"Outlook", "icon":"https://outlook.office365.com/owa/favicon.ico"},
-"googleDrive":{"name":"google drive", "text":"My Drive", "icon":"https://ssl.gstatic.com/images/branding/product/1x/drive_2020q4_32dp.png"},
-"camMath":{"name":"cambridge math", "text":"Cambridge Essential Math", "icon":"https://emac.hotmaths.com.au/users/branding/emacs/img/favicon.ico"}
+"googledrive":{"name":"google drive", "text":"My Drive", "icon":"https://ssl.gstatic.com/images/branding/product/1x/drive_2020q4_32dp.png"},
+"cambridgemath":{"name":"cambridge math", "text":"Cambridge Essential Math", "icon":"https://emac.hotmaths.com.au/users/branding/emacs/img/favicon.ico"}
 }
 document.head = document.head || document.getElementsByTagName('head')[0];
 
@@ -23,8 +23,16 @@ const favicon = document.querySelector('[rel=icon]');
 const amountOfSites = 5
 var count = 0
 function ask() {
+ var text = "Please insert a website\nWebsites:\n",
+     sites = Object.keys(websites)
+ for(let i=0;i<sites.length;i++) {
+  let site = sites[i]
+  text += `${site}\n`
+ }
+ 
+ 
   var site = prompt("Please insert a website (A list of websites can be found on the github page)")
-  var ans = site.toLowerCase()
+  var ans = site.toLowerCase().replace(" ","")
   console.log(ans, site)
 if (site =! null) {
   Object.keys(websites).every(function(key) {
